c7b37fa638573106fd5927852f5e7f913e9b4892
javier.barrientosfer <javier.barrientosfernandez@plexus.es>
2020-11-14 18:17:43 +0100

    refactor: Crear setter getter, imports, exports

--- src/ufosPark.js
+++ src/ufosPark.js
@@ -1,15 +1,22 @@
+import {
+  getterPasta,
+  getterNumeroTarjeta,
+  comprobarPasta,
+} from "./tarjetaDeCredito.js";
+
+export { UfosPark, getterFlota, ufoFlota };
+
 const precioUfo = 500;
 var flotaUfos = {};
 
-function añadirUfo(ufoId) {
-  flotaUfos.null = ufoId;
-  return flotaUfos;
+function UfosPark() {}
+
+var getterFlota = {
+  get flota() {
+    return (this.flotaUfos = flotaUfos);
+  },
+};
+
+function ufoFlota(ufo) {
+  return (flotaUfos[ufo] = null);
 }
-/*
-function dispatch(TarjetaDeCredito) {
-    for(let ufo in flotaUfos) {
-        if (ufo.match != null && TarjetaDeCredito)
-    }
-}
-*/
-console.log(añadirUfo(1));
