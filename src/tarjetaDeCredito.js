2c4ac7103961cc35fd9dc7e94171af27b44e71d5
javier.barrientosfer <javier.barrientosfernandez@plexus.es>
2020-11-15 19:24:17 +0100

    refactor: Eliminar getter, cambiar imports, cambiar nombre metodo

--- src/tarjetaDeCredito.js
+++ src/tarjetaDeCredito.js
@@ -3,26 +3,14 @@ var numeroTarjeta = "";
 var pasta = 3000;
 var tipoDeMoneda = "EZI";
 
-export { TarjetaDeCredito, getterPasta, getterNumeroTarjeta, comprobarPasta };
+export { TarjetaDeCredito, pago, numeroTarjeta, pasta, propietario };
 
 function TarjetaDeCredito(propietario, numeroTarjeta) {
   this.propietario = propietario;
   this.numeroTarjeta = numeroTarjeta;
 }
 
-var getterPasta = {
-  get credito() {
-    return (this.pasta = pasta);
-  },
-};
-
-var getterNumeroTarjeta = {
-  get tarjeta() {
-    return (this.tarjeta = tarjeta);
-  },
-};
-
-function comprobarPasta(pago) {
+function pago(pago) {
   if (pasta >= pago) {
     pasta -= pago;
     return true;
