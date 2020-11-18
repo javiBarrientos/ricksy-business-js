e342215cda88905d3a2f3a3075d5e8b0f37be613
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-11-18 20:44:42 +0100

    refactor: Aplicar prototipos

--- src/tarjetaDeCredito.js
+++ src/tarjetaDeCredito.js
@@ -1,20 +1,36 @@
-var propietario = "";
-var numeroTarjeta = "";
-var pasta = 3000;
-var tipoDeMoneda = "EZI";
-
-export { TarjetaDeCredito, pago, numeroTarjeta, pasta, propietario };
-
+// Constructor
 function TarjetaDeCredito(propietario, numeroTarjeta) {
   this.propietario = propietario;
   this.numeroTarjeta = numeroTarjeta;
+  this.pasta = 3000;
+  this.tipoDeMoneda = "EZI";
 }
 
-function pago(pago) {
-  if (pasta >= pago) {
-    pasta -= pago;
+TarjetaDeCredito.prototype.comprobarPastaPago = function (pago) {
+  if (this.pasta >= pago) {
+    this.pasta -= pago;
     return true;
   } else {
     return false;
   }
+};
+
+/* 
+// Patron singleton, una unica instancia de TarjetaDeCredito
+function singleTarjeta() {
+  const tajetaInstancia = new TarjetaDeCredito();
+
+  return {
+    getTarjeta: function getTarjeta() {
+      return tajetaInstancia;
+    },
+  };
 }
+
+// Export de la variable que devuelve la instancia
+exports.singletonTarjeta = function () {
+  return singleTarjeta();
+};
+ */
+
+export default TarjetaDeCredito;
