baf61436f27ba41f28cb7abced8a4d453bebd824
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-11-20 23:49:46 +0100

    feat: Añadir constructor y metodo dispatch

---
+++ src/crystalExpender.js
@@ -0,0 +1,13 @@
+export default function CrystalExpender(stock, coste) {
+  this.stock = stock;
+  this.coste = coste;
+}
+
+CrystalExpender.prototype.dispatch = function (propietario) {
+  if (this.stock > 0) {
+    if (propietario.pasta >= this.coste) {
+      propietario.pago(this.coste);
+      this.stock -= 1;
+    }
+  }
+};
