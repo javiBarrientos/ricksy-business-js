88ef6540b14868999fbafa183d7343303f3d65da
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-12-06 20:06:58 +0100

    feat: Añadir test comprobar numero

--- src/tarjetaDeCredito/test/tarjetaDeCredito.test.js
+++ src/tarjetaDeCredito/test/tarjetaDeCredito.test.js
@@ -1,7 +1,12 @@
+const { expect } = require("@jest/globals");
 const tarjeta = require("../tarjetaDeCredito");
 
 var testTarjeta = new tarjeta("Maicol", "1234567890");
 
 test("Comprobar tarjeta not null", () => {
   expect(testTarjeta).not.toBeNull();
+});
+
+test("Comprobar numero", () => {
+  expect(testTarjeta.numeroTarjeta).toBe("1234567890");
 });
