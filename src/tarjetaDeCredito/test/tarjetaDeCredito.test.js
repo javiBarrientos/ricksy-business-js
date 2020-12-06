574ab2ed5c71f2d5a6faf90487878900cb70a909
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-12-06 20:21:19 +0100

    feat: Añadir beaforeeach y resto de test

--- src/tarjetaDeCredito/test/tarjetaDeCredito.test.js
+++ src/tarjetaDeCredito/test/tarjetaDeCredito.test.js
@@ -1,7 +1,9 @@
 const { expect } = require("@jest/globals");
 const tarjeta = require("../tarjetaDeCredito");
 
-var testTarjeta = new tarjeta("Maicol", "1234567890");
+beforeEach(() => {
+  testTarjeta = new tarjeta("Maicol", "1234567890");
+});
 
 test("Comprobar tarjeta not null", () => {
   expect(testTarjeta).not.toBeNull();
@@ -9,4 +11,14 @@ test("Comprobar tarjeta not null", () => {
 
 test("Comprobar numero", () => {
   expect(testTarjeta.numeroTarjeta).toBe("1234567890");
+});
+
+test("Comprobar pago", () => {
+  testTarjeta.pago(3000);
+  expect(testTarjeta.pasta).toEqual(0);
+});
+
+test("Comprobar pago error", () => {
+  testTarjeta.pago(4000);
+  expect(testTarjeta.pasta).toBe(3000);
 });
