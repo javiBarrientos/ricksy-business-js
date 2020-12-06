cc8e61868ce7725fb664332fba59209a198e322d
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-12-06 20:00:39 +0100

    feat: Añadir test not null

--- src/tarjetaDeCredito/test/tarjetaDeCredito.test.js
+++ src/tarjetaDeCredito/test/tarjetaDeCredito.test.js
@@ -2,6 +2,6 @@ const tarjeta = require("../tarjetaDeCredito");
 
 var testTarjeta = new tarjeta("Maicol", "1234567890");
 
-test("", () => {
-expect(testTarjeta.numeroTarjeta).toBe("1234567890");
+test("Comprobar tarjeta not null", () => {
+  expect(testTarjeta).not.toBeNull();
 });
